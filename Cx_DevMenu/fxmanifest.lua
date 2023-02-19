3c1bc682e5defbee3748da50131ccf29b8ebe238
Hekson <105570804+Hekson@users.noreply.github.com>
2023-02-19 12:21:08 -0800

    Add files via upload

---
+++ Cx_DevMenu/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "Cx_DevMenu"
+description "Developer Menu"
+author "`Hekson-#6666"
+version "1.0"
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+ui_page 'ui/index.html'
+files {
+	'ui/index.html',
+	'ui/script.js',
+	'ui/css/base.css',
+}
